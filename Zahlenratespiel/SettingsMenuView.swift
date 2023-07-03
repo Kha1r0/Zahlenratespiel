247a5b8a098d3076b577dac34354940292c4c4dc
Kha1r0 <n.kuckeburg@outlook.de>
2023-07-04 00:58:22 +0200

    Initial commit

---
+++ Zahlenratespiel/SettingsMenuView.swift
@@ -0,0 +1,21 @@
+//
+//  SettingsMenu.swift
+//  Zahlenratespiel
+//
+//  Created by Dominik Kuckeburg on 03.07.23.
+//
+
+import Foundation
+import SwiftUI
+
+struct SettingsMenuView: View {
+    var body: some View {
+        Text("Settings")
+    }
+}
+
+struct SettingstMenu_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsMenuView()
+    }
+}
---
+++ Zahlenratespiel/ZahlenratespielApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZahlenratespielApp.swift
+//  Zahlenratespiel
+//
+//  Created by Dominik Kuckeburg on 01.07.23.
+//
+
+import SwiftUI
+
+@main
+struct ZahlenratespielApp: App {
+    var body: some Scene {
+        WindowGroup {
+            StartMenuView()
+        }
+    }
+}
