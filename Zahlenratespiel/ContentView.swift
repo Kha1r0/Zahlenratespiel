07ab9955f82f900e12adff617065b9722c23767d
Kha1r0 <n.kuckeburg@outlook.de>
2024-02-07 00:06:47 +0100

    initial commit

---
+++ Zahlenratespiel/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Zahlenratespiel
+//
+//  Created by Dominik Kuckeburg on 06.02.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Zahlenratespiel/ZahlenratespielApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZahlenratespielApp.swift
+//  Zahlenratespiel
+//
+//  Created by Dominik Kuckeburg on 06.02.24.
+//
+
+import SwiftUI
+
+@main
+struct ZahlenratespielApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
